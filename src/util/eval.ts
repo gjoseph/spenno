b90b01793d5ab998bfeea6351316c0afd3704a3b
Grégory Joseph <greg.jos@gmail.com>
2021-10-30 00:32:26 +1100

    Transpile and eval additionalCheck in rules
    * not ideal way of passing context (or isolating it...)
    * split Rule and RuleDesc to allow passing it to webworker before eval

---
+++ src/util/eval.ts
@@ -0,0 +1,21 @@
+import * as ts from "typescript";
+import { RawRecordFilters } from "../domain/filters";
+
+// https://stackoverflow.com/questions/45153848/evaluate-typescript-from-string
+
+type Fun<A, R> = (args: A) => R;
+
+/**
+ * Evaluates a piece of string, and, if all goes well, returns an executable Function
+ * TODO: is there a better way than "I" (input) to define the function signature?
+ */
+export const evalTSFunction = <I, O>(code: string): Fun<I, O> => {
+  const js = ts.transpile(code);
+
+  // TODO Surely, there is a nicer way to expose functions to eval()
+  // (and isolate it from whatever else is available here)
+  const { isDebit, isCredit, inTime, between } = RawRecordFilters;
+
+  // TODO how do we ensure it has the right types?
+  return eval(js);
+};
