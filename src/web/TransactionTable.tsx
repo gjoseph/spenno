7d6660a29a359dd46eb9af08cdf3791c931891bc
Grégory Joseph <greg.jos@gmail.com>
2021-11-18 19:55:25 +1100

    Extract generic DataGridWrapper so we can reuse the same setup for raw records

--- src/web/TransactionTable.tsx
+++ src/web/TransactionTable.tsx
@@ -1,4 +1,4 @@
-import { DataGrid } from "@mui/x-data-grid";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import * as React from "react";
 import { Bank } from "../domain/accounts";
 import { Transaction } from "../domain/transaction";
@@ -13,33 +13,50 @@ const escapeRegExp = (value: string): string => {
 export const TransactionTable: React.FC<{
   accounts: Bank.Accounts;
   transactions: Transaction[];
-}> = ({ accounts, transactions }) => {
-  const [rows, setRows] = React.useState<Transaction[]>(() => {
-    return transactions;
+}> = (props) => {
+  return (
+    <DataGridWrapper
+      rows={props.transactions}
+      columns={transactionsGridColumns(props.accounts)}
+    />
+  );
+};
+
+const genericRowFilter = (searchRegex: RegExp) => (row: any) => {
+  return Object.keys(row).some((field: any) => {
+    return searchRegex.test(row[field]?.toString());
+  });
+};
+
+type DataGridWrapperProps<T> = {
+  rows: T[];
+  columns: GridColDef[];
+};
+
+export const DataGridWrapper = <T,>(props: DataGridWrapperProps<T>) => {
+  const [rows, setRows] = React.useState<T[]>(() => {
+    // TODO: not sure initialState is needed, since we have a useEffect below that also sets it
+    return props.rows;
   });
 
   const [searchText, setSearchText] = React.useState("");
   const requestSearch = (searchValue: string) => {
     setSearchText(searchValue);
     const searchRegex = new RegExp(escapeRegExp(searchValue), "i");
-    const filteredRows = transactions.filter((row: any) => {
-      return Object.keys(row).some((field: any) => {
-        return searchRegex.test(row[field]?.toString());
-      });
-    });
+    const filteredRows = props.rows.filter(genericRowFilter(searchRegex));
     setRows(filteredRows);
   };
 
   React.useEffect(() => {
-    setRows(transactions);
-  }, [transactions]);
+    setRows(props.rows);
+  }, [props.rows]);
 
   return (
     <React.Fragment>
       <div style={{ display: "flex", height: "100%" }}>
         <div style={{ flexGrow: 1 }}>
           <DataGrid
-            columns={transactionsGridColumns(accounts)}
+            columns={props.columns}
             rows={rows}
             getRowId={(row) => row.id} // Just for clarity, since this is the default behaviour if getRowId is not specified
             pageSize={100}
