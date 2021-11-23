7d68076f1cdea3ba121f66519b18b2d243aa0063
Grégory Joseph <greg.jos@gmail.com>
2021-11-23 23:13:03 +1100

    Add utility to add filename suffix when multiple occurences exist

---
+++ src/util/util.test.ts
@@ -0,0 +1,73 @@
+import {
+  addUniquenessSuffix,
+  addUniquenessSuffixToProperty,
+  addUniquenessSuffixToThings,
+} from "./util";
+
+test("lists with added suffix", () => {
+  const input = ["a", "b", "a", "c", "c", "a"];
+  expect(input.map(addUniquenessSuffix())).toEqual([
+    "a",
+    "b",
+    "a (2)",
+    "c",
+    "c (2)",
+    "a (3)",
+  ]);
+});
+
+test("lists with added suffix using a custom formatter", () => {
+  const input = ["a", "b", "a", "c", "c", "a"];
+  expect(input.map(addUniquenessSuffix((s, i) => `#${i} for ${s}`))).toEqual([
+    "#1 for a",
+    "#1 for b",
+    "#2 for a",
+    "#1 for c",
+    "#2 for c",
+    "#3 for a",
+  ]);
+});
+
+const things = [
+  { name: "a", blah: "foo", count: 123 },
+  { name: "b", blah: "bar", count: 123 },
+  { name: "a", blah: "qux", count: 123 },
+  { name: "c", blah: "qwe", count: 123 },
+  { name: "c", blah: "rty", count: 123 },
+  { name: "a", blah: "uio", count: 123 },
+];
+
+test("list of things added suffix as a mapping function on list", () => {
+  expect(
+    things.map(
+      addUniquenessSuffixToThings(
+        (t) => t.name,
+        (t, s) => ({ ...t, name: s }),
+        (s, i) => `#${i} for ${s}`
+      )
+    )
+  ).toEqual([
+    { name: "#1 for a", blah: "foo", count: 123 },
+    { name: "#1 for b", blah: "bar", count: 123 },
+    { name: "#2 for a", blah: "qux", count: 123 },
+    { name: "#1 for c", blah: "qwe", count: 123 },
+    { name: "#2 for c", blah: "rty", count: 123 },
+    { name: "#3 for a", blah: "uio", count: 123 },
+  ]);
+});
+
+// TODO test that verifies that passing "invalid-prop" to addUniquenessSuffixToProperty() should not compile ? Also, "count" should not compile as its not a string prop
+test("list of things added suffix as a mapping function on list with property", () => {
+  expect(
+    things.map(
+      addUniquenessSuffixToProperty("name", (s, i) => `#${i} for ${s}`)
+    )
+  ).toEqual([
+    { name: "#1 for a", blah: "foo", count: 123 },
+    { name: "#1 for b", blah: "bar", count: 123 },
+    { name: "#2 for a", blah: "qux", count: 123 },
+    { name: "#1 for c", blah: "qwe", count: 123 },
+    { name: "#2 for c", blah: "rty", count: 123 },
+    { name: "#3 for a", blah: "uio", count: 123 },
+  ]);
+});
--- src/util/util.ts
+++ src/util/util.ts
@@ -40,3 +40,63 @@ export const positive = (n: number) => n > 0;
 export const negative = (n: number) => n < 0;
 
 export const zer0 = new Big(0);
+
+// Other random utilities...
+type StringOccurenceCounter = {
+  [key: string]: number;
+};
+type StringOccurenceFormatter = (s: string, c: number) => string;
+
+const defaultSuffixFormat = (s: string, c: number) => {
+  return c > 1 ? `${s} (${c})` : s;
+};
+
+const formatStringsWithCounter =
+  (counts: StringOccurenceCounter, formatter: StringOccurenceFormatter) =>
+  (s: string, idx: number) => {
+    if (counts[s] === undefined) {
+      counts[s] = 1;
+    } else {
+      counts[s]++;
+    }
+    return formatter(s, counts[s]);
+  };
+
+export const addUniquenessSuffix = (
+  formatter: StringOccurenceFormatter = defaultSuffixFormat
+) => {
+  return formatStringsWithCounter({}, formatter);
+};
+
+export const addUniquenessSuffixToThings = <T, U>(
+  extractor: (item: T) => string,
+  replacer: (item: T, suffixedString: string) => U,
+  formatter: StringOccurenceFormatter = defaultSuffixFormat
+) => {
+  const counts: StringOccurenceCounter = {};
+  const formatStrings = formatStringsWithCounter(counts, formatter);
+  return (t: T, idx: number) => {
+    const formattedString = formatStrings(extractor(t), idx);
+    return replacer(t, formattedString);
+  };
+};
+
+// type magic largely inspired by https://stackoverflow.com/questions/46583883/typescript-pick-properties-with-a-defined-type#
+type PickStringProps<T> = {
+  [P in keyof T as T[P] extends string ? P : never]: string;
+};
+
+/**
+ * Simpler function that'll only allow working on a property of T whose value is a string.
+ * Returns a function that can be used in Array.map()
+ */
+export const addUniquenessSuffixToProperty = <T extends PickStringProps<T>>(
+  property: keyof PickStringProps<T>,
+  formatter: StringOccurenceFormatter = defaultSuffixFormat
+) => {
+  return addUniquenessSuffixToThings(
+    (t: T) => t[property],
+    (t, formattedString) => ({ ...t, [property]: formattedString }),
+    formatter
+  );
+};
