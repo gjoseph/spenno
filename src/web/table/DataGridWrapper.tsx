bd5f5ce78342cc96d8a07766a09cd4ec4e0d64ab
Grégory Joseph <greg.jos@gmail.com>
2021-12-04 20:30:14 +1100

    first attempt at sticky table headers

--- src/web/table/DataGridWrapper.tsx
+++ src/web/table/DataGridWrapper.tsx
@@ -44,30 +44,37 @@ export const DataGridWrapper = <T,>(props: DataGridWrapperProps<T>) => {
 
   return (
     <React.Fragment>
-      <div style={{ display: "flex", height: "100%" }}>
-        <div style={{ flexGrow: 1 }}>
-          <DataGrid
-            columns={props.columns}
-            rows={rows}
-            getRowId={(row) => row.id} // Just for clarity, since this is the default behaviour if getRowId is not specified
-            pageSize={100}
-            rowsPerPageOptions={[100]} // single value disables rowsPerPage selector
-            autoHeight
-            density="compact"
-            disableSelectionOnClick
-            disableColumnMenu // we always want all our columns
-            components={{ Toolbar: QuickSearchToolbar }}
-            componentsProps={{
-              toolbar: {
-                value: searchText,
-                onChange: (event: React.ChangeEvent<HTMLInputElement>) =>
-                  requestSearch(event.target.value),
-                clearSearch: () => requestSearch(""),
-              },
-            }}
-            // TODO https://mui.com/components/data-grid/components/#cell has an example that allows a popover to show the full value
-            // value of a cell (e.g for filePath)
-          />
+      {/*
+      this keeps the whole table and tabs at the top of page, headers remain visible and content of table scroll
+      but i'd like to scroll past tabs as well, so that the headers are stuck to the top of the window (or float?)
+      */}
+      <div style={{ height: "100vh", width: "100%" }}>
+        <div style={{ display: "flex", height: "100%" }}>
+          <div style={{ flexGrow: 1 }}>
+            <DataGrid
+              columns={props.columns}
+              rows={rows}
+              getRowId={(row) => row.id} // Just for clarity, since this is the default behaviour if getRowId is not specified
+              pageSize={100}
+              rowsPerPageOptions={[100]} // single value disables rowsPerPage selector
+              // autoHeight
+              // autoPageSize
+              density="compact"
+              disableSelectionOnClick
+              disableColumnMenu // we always want all our columns
+              components={{ Toolbar: QuickSearchToolbar }}
+              componentsProps={{
+                toolbar: {
+                  value: searchText,
+                  onChange: (event: React.ChangeEvent<HTMLInputElement>) =>
+                    requestSearch(event.target.value),
+                  clearSearch: () => requestSearch(""),
+                },
+              }}
+              // TODO https://mui.com/components/data-grid/components/#cell has an example that allows a popover to show the full value
+              // value of a cell (e.g for filePath)
+            />
+          </div>
         </div>
       </div>
     </React.Fragment>
