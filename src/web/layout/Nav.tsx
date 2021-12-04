d37d4a2ac16fbcc49ec7a7dcf3bc567891aa3bde
Grégory Joseph <greg.jos@gmail.com>
2021-12-04 18:38:31 +1100

    Slight changes to app title, introduce smaller components in TopBar for simplicity

--- src/web/layout/Nav.tsx
+++ src/web/layout/Nav.tsx
@@ -15,7 +15,40 @@ import DialogTitle from "@mui/material/DialogTitle";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import * as React from "react";
-import { MouseEventHandler } from "react";
+import { MouseEventHandler, useMemo } from "react";
+
+type IconAndDialogContent = {
+  icon: React.JSXElementConstructor<any>;
+  title: React.ReactNode;
+  content?: React.ReactNode;
+  onClick?: (() => void) | (() => Promise<void>) | undefined;
+};
+
+type IconAndOnClick = {
+  icon: React.JSXElementConstructor<any>;
+  title: React.ReactNode;
+  onClick: () => void | Promise<void>;
+};
+
+const AppTitle: React.FC<{ icon: React.JSXElementConstructor<any> }> = (
+  props
+) => (
+  <React.Fragment>
+    <props.icon
+      sx={{ position: "relative", padding: 0, mr: "13px" }}
+      fontSize="large"
+    />
+    <Typography
+      component="h1"
+      variant="h5"
+      color="inherit"
+      noWrap
+      sx={{ flexGrow: 1 }}
+    >
+      {props.children}
+    </Typography>
+  </React.Fragment>
+);
 
 const IconButton: React.FC<{
   icon: React.JSXElementConstructor<any>;
@@ -32,12 +65,41 @@ const IconButton: React.FC<{
   </MuiIconButton>
 );
 
-type IconAndDialogContent = {
+const AppBarButtons = ({
+  iconAndOnClicks,
+}: {
+  iconAndOnClicks: IconAndOnClick[];
+}) => (
+  <ButtonGroup color="inherit">
+    {iconAndOnClicks.map((i, idx) => {
+      return <IconButton icon={i.icon} onClick={i.onClick} key={idx} />;
+    })}
+  </ButtonGroup>
+);
+
+const AppSpeedDial: React.FC<{
   icon: React.JSXElementConstructor<any>;
-  title: React.ReactNode;
-  content?: React.ReactNode;
-  onClick?: (() => void) | (() => Promise<void>) | undefined;
-};
+  iconAndOnClicks: IconAndOnClick[];
+}> =
+  // Need to use forwardRef so we can use this component in <Slide> below
+  React.forwardRef((props, ref) => (
+    <SpeedDial
+      ref={ref}
+      ariaLabel="Quick access menu"
+      // positioning is only needed because of the slide
+      sx={{ position: "fixed", bottom: 16, right: 16 }}
+      icon={<SpeedDialIcon icon={<props.icon />} />}
+    >
+      {props.iconAndOnClicks.map((i, idx) => (
+        <SpeedDialAction
+          key={idx}
+          icon={<i.icon />}
+          tooltipTitle={i.title}
+          onClick={i.onClick}
+        />
+      ))}
+    </SpeedDial>
+  ));
 
 export const TopBar: React.FC<{
   appTitle: React.ReactNode;
@@ -47,6 +109,16 @@ export const TopBar: React.FC<{
   const [open, setOpen] = React.useState<number>(-1);
   const handleOpen = (idx: number) => () => setOpen(idx);
   const handleClose = () => setOpen(-1);
+
+  const iconAndOnClicks: IconAndOnClick[] = useMemo(
+    () =>
+      props.iconAndDialogs.map((i, idx) => ({
+        ...i,
+        onClick: i.onClick || handleOpen(idx),
+      })),
+    [props.iconAndDialogs]
+  );
+
   const scrollTrigger = useScrollTrigger({
     // disableHysteresis: true, // i don't really understand what this is
     threshold: 0,
@@ -57,30 +129,8 @@ export const TopBar: React.FC<{
       <Slide appear={false} direction="down" in={!scrollTrigger}>
         <AppBar>
           <Toolbar>
-            <Typography
-              component="h1"
-              variant="h6"
-              color="inherit"
-              noWrap
-              sx={{ flexGrow: 1 }}
-            >
-              {props.appTitle}
-              <props.appIcon
-                sx={{ position: "relative", bottom: -10 }}
-                fontSize="large"
-              />
-            </Typography>
-            <ButtonGroup color="inherit">
-              {props.iconAndDialogs.map((i, idx) => {
-                return (
-                  <IconButton
-                    icon={i.icon}
-                    onClick={i.onClick || handleOpen(idx)}
-                    key={idx}
-                  />
-                );
-              })}
-            </ButtonGroup>
+            <AppTitle icon={props.appIcon}>{props.appTitle}</AppTitle>
+            <AppBarButtons iconAndOnClicks={iconAndOnClicks} />
 
             {/* TODO we could make these draggable and keep open https://mui.com/components/dialogs/#draggable-dialog*/}
             {props.iconAndDialogs.map((i, idx) => (
@@ -90,27 +140,12 @@ export const TopBar: React.FC<{
               </Dialog>
             ))}
           </Toolbar>
-
           {props.children}
         </AppBar>
       </Slide>
 
       <Slide direction="up" in={scrollTrigger}>
-        <SpeedDial
-          ariaLabel="Quick access menu"
-          // positioning is only needed because of the slide
-          sx={{ position: "fixed", bottom: 16, right: 16 }}
-          icon={<SpeedDialIcon icon={<props.appIcon />} />}
-        >
-          {props.iconAndDialogs.map((i, idx) => (
-            <SpeedDialAction
-              key={idx}
-              icon={<i.icon />}
-              tooltipTitle={i.title}
-              onClick={i.onClick || handleOpen(idx)}
-            />
-          ))}
-        </SpeedDial>
+        <AppSpeedDial icon={props.appIcon} iconAndOnClicks={iconAndOnClicks} />
       </Slide>
     </React.Fragment>
   );
