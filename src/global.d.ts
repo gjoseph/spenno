66349cbce49787e86ccd3cf243eb9b73365a1c21
Grégory Joseph <greg.jos@gmail.com>
2021-12-30 23:25:12 +1100

    Add type definitions for toEqualMoment
    * Not sure if/how this worked before either...

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,15 +1,26 @@
-// https://github.com/developit/workerize-loader/issues/3#issuecomment-538730979
-declare module "workerize-loader!*" {
-  type AnyFunction = (...args: any[]) => any;
-  type Async<F extends AnyFunction> = (
-    ...args: Parameters<F>
-  ) => Promise<ReturnType<F>>;
+import { Moment } from "moment";
 
-  type Workerized<T> = Worker & {
-    [K in keyof T]: T[K] extends AnyFunction ? Async<T[K]> : never;
-  };
+declare global {
+  // https://github.com/developit/workerize-loader/issues/3#issuecomment-538730979
+  module "workerize-loader!*" {
+    type AnyFunction = (...args: any[]) => any;
+    type Async<F extends AnyFunction> = (
+      ...args: Parameters<F>
+    ) => Promise<ReturnType<F>>;
 
-  function createInstance<T>(): Workerized<T>;
+    type Workerized<T> = Worker & {
+      [K in keyof T]: T[K] extends AnyFunction ? Async<T[K]> : never;
+    };
 
-  export = createInstance;
+    function createInstance<T>(): Workerized<T>;
+
+    export = createInstance;
+  }
+
+  // Register jest/toEqualMoment
+  namespace jest {
+    interface Matchers<R> {
+      toEqualMoment(m: Moment): R;
+    }
+  }
 }
